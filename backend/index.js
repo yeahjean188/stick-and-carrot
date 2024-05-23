a94ff19380662dfddf8b3fdd5567c66de348a451
유예진 <whjzfk@gmail.com>
2024-05-23 13:21:22 +0900

    gpt 모델 변경

--- backend/index.js
+++ backend/index.js
@@ -79,7 +79,7 @@ app.post('/fortuneTell', async function (req, res) {
 
     const completion = await openai.chat.completions.create({
         messages: messages,
-        model: "gpt-3.5-turbo"
+        model: "gpt-4.o-turbo"
     });
 
     let fortune = completion.choices[0].message['content'];
